beee1b1563f15cead88b4a07de3ec558efab8d94
Alexis Trevizo <At2848@nyu.edu>
2020-09-24 22:30:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-document.querySelectorAll("button").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    gsap.to(window, {
-      duration: 1,
-      ease: Expo.easeOut,
-      scrollTo: { y: ".heading-2", offsetY: 70 }
-    });
+document.getElementById("workButton").addEventListener("click", function () {
+  console.log("hola");
+  gsap.to(window, {
+    duration: 1,
+    ease: Expo.easeOut,
+    scrollTo: { y: ".heading-2", offsetY: 70 }
   });
 });
 
+document.getElementById("section1Btn").addEventListener("click", function () {
+  console.log("hola");
+  gsap.to(window, {
+    duration: 1,
+    ease: Expo.easeOut,
+    scrollTo: { y: ".landing", offsetY: 70 }
+  });
+});
 
+gsap.registerPlugin(ScrollTrigger);
+
+var timeline = gsap.timeline();
+gsap.from("#section1Btn", {
+  y: 100,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".heading-2",
+    scrub: true,
+    // markers: true,
+    start: "top center",
+    end: "top top"
+  }
+});
